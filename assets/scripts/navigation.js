ceaf1dec93bd1378ad9dc1c9fcb5dc4b504827c5
adidinu <adriandinu3@yahoo.com>
2022-04-23 03:21:03 +0300

    home page + poze

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,14 +1,27 @@
-const menus = document.querySelectorAll('.js-open-menu')
+const menus = document.querySelectorAll('.js-open-menu');
+const navWrapper = document.querySelector('.nav-wrapper');
+const subMenus = document.querySelectorAll('.sub-menu');
 
-for(let i = 0; i < menus.length; i++){
-  menus[i].addEventListener('mouseover', ()=>{
-    const rel = menus[i].closest('.js-menu-rel')
-    const subMenu = rel.querySelector('.sub-menu');
-    if(subMenu){
-      subMenu.classList.add('show');
-      subMenu.addEventListener('mouseleave', ()=>{
-        subMenu.classList.remove('show');
-      })
-    }
-  })
-}
+navWrapper.addEventListener('mouseover', (e) => {
+	if (!e.target.closest('.js-open-menu') && !e.target.closest('.sub-menu')) {
+		subMenus.forEach((subMenu) => {
+			subMenu.classList.remove('show');
+		});
+	}
+});
+
+menus.forEach((menu) => {
+	menu.addEventListener('mouseover', () => {
+		subMenus.forEach((subMenu) => {
+			subMenu.classList.remove('show');
+		});
+		const rel = menu.closest('.js-menu-rel');
+		const subMenu = rel.querySelector('.sub-menu');
+		if (subMenu) {
+			subMenu.classList.add('show');
+			subMenu.addEventListener('mouseleave', () => {
+				subMenu.classList.remove('show');
+			});
+		}
+	});
+});
