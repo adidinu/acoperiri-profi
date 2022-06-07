8b7dbf0bea8e09a8a559f57ad9acbb0bc65397a7
adriandinug <adriandinu3@yahoo.com>
2022-06-07 23:30:43 +0300

    ce am mai facut

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,6 +1,9 @@
 const menus = document.querySelectorAll('.js-open-menu');
 const navWrapper = document.querySelector('.nav-wrapper');
 const subMenus = document.querySelectorAll('.sub-menu');
+const mobileMenuToggle = document.querySelector('.open-menu');
+const mobileNav = document.querySelector('.mobile-popup');
+const closeMobileMenu = document.querySelector('.close-menu');
 
 navWrapper.addEventListener('mouseleave', (e) => {
   subMenus.forEach((subMenu) => {
@@ -24,4 +27,24 @@ menus.forEach((menu) => {
 	});
 });
 
+const hideNav = (e) => {
+  if(!e.target.closest('.open-menu') && !e.target.closest('.mobile-popup')){
+    mobileNav.classList.remove('show');
+    document.body.style.overflow = "";
+    document.body.classList.remove("overlay");
+  }
+}
 
+mobileMenuToggle.addEventListener('click', () => {
+  mobileNav.classList.add('show');
+  document.body.style.overflow = "hidden";
+  document.body.classList.add("overlay");
+});
+
+document.addEventListener('click', hideNav);
+
+closeMobileMenu.addEventListener('click', (e) =>{
+  mobileNav.classList.remove('show');
+  document.body.style.overflow = "";
+  document.body.classList.remove("overlay");
+})
