1496642dbfde09d2719797abf19da80e64bc6d84
adriandinug <adriandinu3@yahoo.com>
2022-06-07 23:33:26 +0300

    removed nav scripts

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,31 +1,6 @@
-const menus = document.querySelectorAll('.js-open-menu');
-const navWrapper = document.querySelector('.nav-wrapper');
-const subMenus = document.querySelectorAll('.sub-menu');
 const mobileMenuToggle = document.querySelector('.open-menu');
 const mobileNav = document.querySelector('.mobile-popup');
 const closeMobileMenu = document.querySelector('.close-menu');
-
-navWrapper.addEventListener('mouseleave', (e) => {
-  subMenus.forEach((subMenu) => {
-    subMenu.classList.remove('show');
-  });
-});
-
-menus.forEach((menu) => {
-	menu.addEventListener('mouseover', () => {
-		subMenus.forEach((subMenu) => {
-			subMenu.classList.remove('show');
-		});
-		const rel = menu.closest('.js-menu-rel');
-		const subMenu = rel.querySelector('.sub-menu');
-		if (subMenu) {
-			subMenu.classList.add('show');
-			subMenu.addEventListener('mouseleave', () => {
-				subMenu.classList.remove('show');
-			});
-		}
-	});
-});
 
 const hideNav = (e) => {
   if(!e.target.closest('.open-menu') && !e.target.closest('.mobile-popup')){
