c8f09aeacdc34605ec21275b432d26a1fc962ca8
adriandinug <adriandinu3@yahoo.com>
2022-06-08 01:32:13 +0300

    fix nav

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,6 +1,8 @@
 const mobileMenuToggle = document.querySelector('.open-menu');
 const mobileNav = document.querySelector('.mobile-popup');
 const closeMobileMenu = document.querySelector('.close-menu');
+const navWrapper = document.querySelector('.nav-wrapper');
+const header = document.querySelector('header');
 
 const hideNav = (e) => {
   if(!e.target.closest('.open-menu') && !e.target.closest('.mobile-popup')){
@@ -22,4 +24,15 @@ closeMobileMenu.addEventListener('click', (e) =>{
   mobileNav.classList.remove('show');
   document.body.style.overflow = "";
   document.body.classList.remove("overlay");
+})
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > header.offsetHeight){
+    navWrapper.classList.add('sticky');
+    document.querySelector('header + *').style.paddingTop = navWrapper.offsetHeight + "px";
+  }
+  else{
+    navWrapper.classList.remove('sticky');
+    document.querySelector('header + *').style.paddingTop = "";
+  }
 })
